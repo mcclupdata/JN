2bb5ee3b60bcfb4bc68562908e3e557075382a8f
iepjunlin <iepjunlin@shiep.edu.cn>
2016-03-15 13:55:36 +0800

    客户端而是GIT同步

--- frmMain.designer.cs
+++ frmMain.designer.cs
@@ -65,6 +65,7 @@
             this.efButton34 = new EF.EFButton();
             this.efButton35 = new EF.EFButton();
             this.efButton36 = new EF.EFButton();
+            this.efButton37 = new EF.EFButton();
             ((System.ComponentModel.ISupportInitialize)(this.efButton1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.efButton2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.efButton3)).BeginInit();
@@ -101,6 +102,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.efButton34)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.efButton35)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.efButton36)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.efButton37)).BeginInit();
             this.SuspendLayout();
             // 
             // efButton1
@@ -682,11 +684,27 @@
             this.efButton36.ViewMode = EF.ViewModeEnum.Enable;
             this.efButton36.Click += new System.EventHandler(this.efButton36_Click);
             // 
+            // efButton37
+            // 
+            this.efButton37.Authorizable = false;
+            this.efButton37.EnabledEx = true;
+            this.efButton37.FnNo = 0;
+            this.efButton37.Hint = "";
+            this.efButton37.Location = new System.Drawing.Point(1224, 288);
+            this.efButton37.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.efButton37.Name = "efButton37";
+            this.efButton37.Size = new System.Drawing.Size(161, 74);
+            this.efButton37.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.efButton37.TabIndex = 40;
+            this.efButton37.Text = "测试";
+            this.efButton37.ViewMode = EF.ViewModeEnum.Enable;
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1398, 755);
+            this.Controls.Add(this.efButton37);
             this.Controls.Add(this.efButton36);
             this.Controls.Add(this.efButton35);
             this.Controls.Add(this.efButton34);
@@ -762,6 +780,7 @@
             this.Controls.SetChildIndex(this.efButton34, 0);
             this.Controls.SetChildIndex(this.efButton35, 0);
             this.Controls.SetChildIndex(this.efButton36, 0);
+            this.Controls.SetChildIndex(this.efButton37, 0);
             ((System.ComponentModel.ISupportInitialize)(this.efButton1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.efButton2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.efButton3)).EndInit();
@@ -798,6 +817,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.efButton34)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.efButton35)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.efButton36)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.efButton37)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -841,5 +861,6 @@
         private EF.EFButton efButton34;
         private EF.EFButton efButton35;
         private EF.EFButton efButton36;
+        private EF.EFButton efButton37;
     }
 }
